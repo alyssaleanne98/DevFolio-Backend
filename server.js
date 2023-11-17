49e0d6d1651a7bd580ef0977f77c2e26bfbfcba0
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-11-17 12:46:37 -0800

    server listener working

--- server.js
+++ server.js
@@ -5,3 +5,20 @@ const cors = require("cors");
 // Import JSON files 
 const projects = require("./projects.json");
 const about = require("./about.json");
+
+//Create our app object 
+const app = express();
+
+//set up middleware
+app.use(cors());
+
+//home route for testing our app 
+app.get("/", (req, res) => {
+    res.send("Hello Alyssa")
+});
+
+
+//declare a variable for our port number
+const PORT = process.env.PORT || 4000;
+//turn on the server listener 
+app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
